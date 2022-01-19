e55bc444c067e46c484e8d86d200492e7bf91a41
Mohamed Ali <12maali27@gmail.com>
2022-01-19 14:03:37 -0500

    create component that shows the layout for one interviewer

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import classNames from "classnames";
+import "./InterviewerListItem.scss";
+
+const InterviewerListItem = (props) => {
+  console.log(props);
+  let styles = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+  return (
+    <li className={styles} onClick={props.setInterviewer}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+};
+
+export default InterviewerListItem;
