1f7ada04a009742626e85f41c8912ef728f992dd
Mohamed Ali <12maali27@gmail.com>
2022-01-20 19:20:03 -0500

    import Header, Show, Empty components and add conditional rendering

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,22 @@
 import React from "react";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 import "./styles.scss";
 
 export default function Appointment(props) {
+  console.log(props);
   return (
     <article className="appointment">
-      {!props.time && <p>No Appointments</p>}
-      {props.time && <p>Appointment at {props.time}</p>}
+      <Header time={props.time} />
+      {!props.interview && <Empty />}
+      {props.interview && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
     </article>
   );
 }
