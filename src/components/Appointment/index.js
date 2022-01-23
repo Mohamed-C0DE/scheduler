233c324c9a564a6be4a44a22a4d92b5b853c3f52
Mohamed Ali <12maali27@gmail.com>
2022-01-22 22:07:31 -0500

    import useVisualMode custom hook and conditional render components based on custom hook mode state

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,19 +2,32 @@ import React from "react";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode";
 
 import "./styles.scss";
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+  console.log(props);
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {!props.interview && <Empty />}
-      {props.interview && (
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
+      )}
+      {mode === CREATE && (
+        <Form interviewers={props.interviewers} onCancel={back} />
       )}
     </article>
   );
